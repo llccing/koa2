e8470689c206a702b91cb88b813690f28ae80fe9
liuchunfeng <liuchunfeng@jd.com>
2017-09-29 16:30:02 +0800

    init

---
+++ routes/users.js
@@ -0,0 +1,67 @@
+const router = require('koa-router')()
+
+router.prefix('/users')
+
+router.get('/', function (ctx, next) {
+  console.log(ctx);
+  console.log(next);
+  var obj = {
+    "code": 200,
+    "message": "",
+    "data": [{
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }, {
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }, {
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }, {
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }, {
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }, {
+      "sn": "在线",
+      "ip": 123,
+      "info": "CPU故障"
+    }]
+  };
+
+  ctx.body = obj;
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+
+})
+
+router.get('/list', function (ctx, next) {
+  let data = [];
+
+  console.log(ctx.query)
+  let { currentPage, pageSize } = ctx.query;
+
+  for (let i = 1; i <= pageSize; i++) {
+    data.push({
+      id: currentPage + i,
+      name: 'name' + i
+    })
+  }
+
+  ctx.body = {
+    data: data,
+    currentPage:currentPage,
+    totalPages: 100,
+  }
+
+})
+
+module.exports = router
